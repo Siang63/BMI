a22b1e9a0e1020156dd02662ead2728d7f895a04
m3072\w401 <hiiragi199963@gmail.com>
2018-03-15 17:51:49 +0800

    frist commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("╔═══════╗");
+            Console.WriteLine("║    D M A     ║");
+            Console.WriteLine("╚═══════╝");
+
+            // double rate = 4.61931155;
+            //  Console.WriteLine("目前人民幣兌台幣：" + rate);
+            //  Console.Write("請輸入人民幣金額：");
+            //  string input = Console.ReadLine();
+
+            //  double total = int.Parse(input) * rate;
+            //   Console.WriteLine("等於台幣：" + total);
+            Console.ReadLine();
+        }
+    }
+}
